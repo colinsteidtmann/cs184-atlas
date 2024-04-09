60cae6018ca8ab57966063d7a52a35696f2c8f2d
Ian Dong <ihdong@berkeley.edu>
2024-04-08 21:21:07 -0700

    Added functionality to jump up and down

--- src/main.cpp
+++ src/main.cpp
@@ -626,6 +626,10 @@ void processInput(GLFWwindow *window, Shader &shader) {
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.ProcessKeyboard(UP, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera.ProcessKeyboard(DOWN, deltaTime);
 }
 
 void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
