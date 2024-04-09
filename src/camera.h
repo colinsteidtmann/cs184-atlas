e7d84989b4d210c0c373fb5fe96fd184bc0ad250
Ian Dong <ihdong@berkeley.edu>
2024-04-08 21:23:51 -0700

    Added changes to move camera up and down

--- src/camera.h
+++ src/camera.h
@@ -79,6 +79,10 @@ public:
             Position -= Right * velocity;
         if (direction == RIGHT)
             Position += Right * velocity;
+        if (direction == UP)
+            Position += Up * velocity;
+        if (direction == DOWN)
+            Position -= Up * velocity;
     }
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
